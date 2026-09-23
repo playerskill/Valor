add7022c32c47bbbcdb2514921db333ccd475631
playerskill <96878065+playerskill@users.noreply.github.com>
2026-09-24 00:14:04 +0300

    Create LogWriterCommand.java

---
+++ scr/main/java/ru/player_skill/logwriter/LogWriterCommand.java
@@ -0,0 +1,30 @@
+package ru.playerskill.logwriter;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public final class LogWriterCommand implements CommandExecutor {
+    private final LogWriter plugin;
+
+    public LogWriterCommand(LogWriter plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("logwriter.reload")) {
+            sender.sendMessage(plugin.getMessage("messages.no-permission"));
+            return true;
+        }
+
+        if (args.length != 1 || !args[0].equalsIgnoreCase("reload")) {
+            sender.sendMessage(plugin.getMessage("messages.reload-usage"));
+            return true;
+        }
+
+        plugin.reloadConfig();
+        sender.sendMessage(plugin.getMessage("messages.reload-success"));
+        return true;
+    }
+}
